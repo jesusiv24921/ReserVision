49097b255815389aa963dae503af60fddbe0d8a1
jesusiv24921 <jesus.pacheco.rom@gmail.com>
2024-10-17 23:22:00 -0500

    Update code and requirements

--- my_project/scripts/auth.py
+++ my_project/scripts/auth.py
@@ -3,7 +3,7 @@ import dash_auth
 # Lista de usuarios y contraseñas (esto debe almacenarse de forma segura en una aplicación real)
 VALID_USERNAME_PASSWORD_PAIRS = {
     'jesuspacheco': '26Amarillo3*',
-    'user2': 'password2'
+    'visitante': '26Rojo3*'
 }
 
 def authenticate(app):
